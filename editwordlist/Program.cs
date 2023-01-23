5d802827005dd20f05eafc895c897b8e34f2e121
ChrMonMor <97163196+ChrMonMor@users.noreply.github.com>
2023-01-23 15:27:32 +0100

    Add project files.

---
+++ editwordlist/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace editwordlist
+{
+    public class Program
+    {
+        private static readonly string path = @"C:\Users\chri615w\source\repos\ordspil\editwordlist\oldlist\2012dkk.txt";
+        static void Main(string[] args)
+        {
+            
+            foreach (string line in WordListDansk(0, 255))
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+
+        }
+        // virker kun men 2012dansk 
+        private static string[] RemoveFlawsFromArray(string[] readText)
+        {
+            Regex r = new Regex("[0-9]$");
+            Array.Sort(readText);
+            for (int i = 0; i < readText.Length; i++)
+            {
+                readText[i] = readText[i].ToLower();
+                int j = readText[i].IndexOf(';');
+                if (readText[i][j - 1] == '.')
+                {
+                    j--;
+                }
+                readText[i] = readText[i].Remove(j, readText[i].Length - j);
+                readText[i] = readText[i].Replace("-", "");
+                if (readText[i].Any(char.IsDigit))
+                {
+                    readText[i] = readText[i].Remove(0, 3);
+                }
+            }
+            readText = readText.Distinct().ToArray();
+            return readText = readText.Where(x => !x.Contains(" ")).ToArray();
+        }
+        public static string[] WordListDansk(int min, int max)
+        {
+            string[] readText = File.ReadAllLines(path);
+            readText = RemoveFlawsFromArray(readText);
+            return readText = readText.Where(x=>x.Length>min && x.Length<max).ToArray();
+        }
+
+    }
+}
---
+++ ordspil/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using editwordlist;
+
+namespace ordspil.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string secretWord = "";
+
+        public string SecretWord { get => secretWord; set => secretWord = value; }
+
+        public ActionResult Index()
+        {
+            string[] ordArray = editwordlist.Program.WordListDansk(3, 5);
+
+            Random random = new Random();
+            secretWord = ordArray[random.Next(0, ordArray.Length)];
+
+            ViewBag.SecretWordLenght = secretWord.Length;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult index(string userGuess)
+        {
+
+            return Content("hyrrat : "+ SecretWord + " : " + 1);
+        }
+    }
+}
